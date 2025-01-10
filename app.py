7a744a6574b1fa39dc06293964e00da4b54aaf64
JuanAnacona15 <juanmanuelanaconamorales@gmail.com>
2025-01-10 14:19:20 -0500

    Add page for send readings and query invoices in pdf format.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response, send_from_directory
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 import mysql.connector
+from xhtml2pdf import pisa
+from jinja2 import Environment, FileSystemLoader
 import decimal
 
 app = Flask(__name__)
@@ -21,21 +23,146 @@ def get_db_connection():
 @app.route('/reading', methods=['POST'])
 def add_reading():
     try:
+        if not request.is_json:
+            return jsonify({"error": "Content-Type must be application/json"}), 400
+        
         data = request.json
-        contract_number = data['contract_number']
-        kw_consumed = data['kw_consumed']
-        date = datetime.now()
-        reading = [contract_number, date, kw_consumed]
 
+        required_fields = ['kw_consumed', 'counter']
+        for field in required_fields:
+            if field not in data:
+                return jsonify({"error": f"Missing required field: {field}"}), 400
+
+        if not isinstance(data['kw_consumed'], (int, float)):
+            return jsonify({"error": "kw_consumed must be a number"}), 400
+        if not isinstance(data['counter'], str):
+            return jsonify({"error": "counter must be an integer"}), 400
+            
+        date = datetime.now()
+        
         con = get_db_connection()
         cursor = con.cursor()
-        query = "INSERT INTO reading (contract_id, date, kw_consumed) VALUES(%s, %s, %s)"
-        cursor.execute(query, reading)
+
+        cursor.execute("SELECT id_contract FROM contracts WHERE counter = %s LIMIT 1;", (str(data['counter']),))
+        id_contract = cursor.fetchone()[0]
+        print(id_contract)
+        query = "INSERT INTO reading (contract_id, date, kw_consumed, counter) VALUES (%s, %s, %s, %s)"
+        
+        cursor.execute(query, (
+            id_contract, 
+            date, 
+            data['kw_consumed'], 
+            data['counter']
+        ))
+        
         con.commit()
 
-        return jsonify({"message": "Reading added successfully"}), 201
+        return jsonify({
+            "message": "Reading added successfully",
+            "data": {
+                "contract_id": id_contract,
+                "date": date.isoformat(),
+                "kw_consumed": data['kw_consumed'],
+                "counter": data['counter']
+            }
+        }), 201
+        
     except mysql.connector.Error as err:
-        return jsonify({"error": str(err)}), 500
+        error_message = str(err)
+        if err.errno == 1062: 
+            return jsonify({"error": "Duplicate reading entry"}), 409
+        elif err.errno == 1452:
+            return jsonify({"error": "Invalid contract_id"}), 400
+        else:
+            return jsonify({"error": error_message}), 500
+            
+    except Exception as e:
+        return jsonify({"error": "Internal server error"}), 500
+        
+    finally:
+        if 'cursor' in locals() and cursor is not None:
+            cursor.close()
+        if 'con' in locals() and con is not None and con.is_connected():
+            con.close()
+
+def fetch_invoice_data(id_contract):
+    try:
+        con = get_db_connection()
+        cursor = con.cursor()
+
+        query = """
+                WITH latest_invoice AS (
+                    SELECT *
+                    FROM invoice
+                    WHERE id_contract = %s
+                    ORDER BY issue_date DESC
+                    LIMIT 1
+                )
+                SELECT 
+                    li.invoice_id,
+                    c.name AS contract_name,
+                    c.id,
+                    c.commune,
+                    c.address,
+                    li.value AS invoice_value,
+                    li.invoiced_period,
+                    li.issue_date,
+                    r.id_reading,
+                    r.date AS reading_date,
+                    li.kw_cost,
+                    li.public_lighting,
+                    r.kw_consumed,
+                    li.financing,
+                    c.telephone,
+                    c.stratum,
+                    c.counter,
+                    c.user_points
+                    
+                FROM latest_invoice li
+                JOIN reading r ON li.id_reading = r.id_reading
+                JOIN contracts c ON li.id_contract = c.id_contract;
+        """
+        cursor.execute(query, (id_contract,))
+        invoice_data = cursor.fetchone()
+
+        cost_energy = invoice_data[10] * invoice_data[12]
+        subtotal = cost_energy - invoice_data[13]
+        rounding = subtotal - int(subtotal)
+        suspension_date = invoice_data[7] + relativedelta(days=7)
+        next_reading = invoice_data[7] + relativedelta(days=28)
+
+        if invoice_data:
+            invoice_dict = {
+                "invoice_id": invoice_data[0],
+                "id_contract" : id_contract,
+                "contract_name": invoice_data[1],
+                "id": invoice_data[2],
+                "commune": invoice_data[3],
+                "address": invoice_data[4],
+                "invoice_value": invoice_data[5],
+                "invoiced_period": invoice_data[6],
+                "issue_date": invoice_data[7].strftime('%Y-%m-%d'),
+                "id_reading": invoice_data[8],
+                "reading_date": invoice_data[9].strftime('%Y-%m-%d'),
+                "kw_cost": invoice_data[10],
+                "public_lighting": invoice_data[11],
+                "kw_consumed": invoice_data[12],
+                "financing": invoice_data[13],
+                "telephone": invoice_data[14],
+                "stratum": invoice_data[15],
+                "counter": invoice_data[16],
+                "user_points": invoice_data[17],
+                "cost_energy": cost_energy,
+                "subtotal": subtotal,
+                "rounding": rounding,
+                "suspension_date": suspension_date.strftime('%Y-%m-%d'),
+                "next_reading": next_reading.strftime('%Y-%m-%d')
+            }
+            return invoice_dict
+        else:
+            return None
+    except mysql.connector.Error as err:
+        return err
     finally:
         if 'con' in locals() and con.is_connected():
             con.close()
@@ -57,7 +184,7 @@ def generate_invoice(id_contract):
 
         cost_energy = previous_record[3] * general_values[contract_data[6] - 1][1]
         financing = previous_record[3] * general_values[contract_data[6] - 1][2]
-        subtotal = cost_energy + financing
+        subtotal = cost_energy - financing
         rounding = subtotal - int(subtotal)
         total = subtotal - rounding + general_values[contract_data[6] - 1][3]
 
@@ -66,11 +193,9 @@ def generate_invoice(id_contract):
         else:
             invoiced_period = "First invoice"
         expedition_date = datetime.now()
-        suspension_date = expedition_date + relativedelta(days=7)
-        next_reading = datetime.now() + relativedelta(days=28)
 
-        cursor.execute("INSERT INTO invoice (id_contract, value, invoiced_period, issue_date, id_reading, kw_cost, financing) VALUES (%s, %s, %s, %s, %s, %s, %s);",
-                       (contract_data[0], total, invoiced_period, expedition_date, previous_record[0], general_values[contract_data[6] - 1][1], financing))
+        cursor.execute("INSERT INTO invoice (id_contract, value, invoiced_period, issue_date, id_reading, kw_cost, financing, public_lighting) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);",
+                       (contract_data[0], total, invoiced_period, expedition_date, previous_record[0], general_values[contract_data[6] - 1][1], financing, general_values[contract_data[6] - 1][3]))
         con.commit()
 
         return jsonify({"message": "Invoice generated successfully"}), 201
@@ -83,81 +208,41 @@ def generate_invoice(id_contract):
 @app.route('/getinvoice/<int:id_contract>')
 def get_invoice(id_contract):
     try:
-        con = get_db_connection()
-        cursor = con.cursor()
-
-        query = """
-                WITH latest_invoice AS (
-                    SELECT *
-                    FROM invoice
-                    WHERE id_contract = %s
-                    ORDER BY issue_date DESC
-                    LIMIT 1
-                )
-                -- Step 2: Get the reading and contract details based on the invoice
-                SELECT 
-                    li.invoice_id,
-                    c.name AS contract_name,
-                    c.id,
-                    c.commune,
-                    c.address,
-                    li.value AS invoice_value,
-                    li.invoiced_period,
-                    li.issue_date,
-                    r.date AS reading_date,
-                    li.kw_cost,
-                    r.kw_consumed,
-                    li.financing,
-                    c.telephone,
-                    c.stratum,
-                    c.counter,
-                    c.user_points
-                    
-                FROM latest_invoice li
-                JOIN reading r ON li.id_reading = r.id_reading
-                JOIN contracts c ON li.id_contract = c.id_contract;
-        """
-        cursor.execute(query, (id_contract,))
-        invoice_data = cursor.fetchone()
-
-        cost_energy = invoice_data[9] * invoice_data[10]
-        subtotal = cost_energy + invoice_data[11]
-        rounding = subtotal - int(subtotal)
-        suspension_date = invoice_data[7] + relativedelta(days=7)
-        next_reading = invoice_data[7] + relativedelta(days=28)
+        invoice_data = fetch_invoice_data(id_contract)
 
         if invoice_data:
-            invoice_dict = {
-                "invoice_id": invoice_data[0],
-                "contract_name": invoice_data[1],
-                "id": invoice_data[2],
-                "commune": invoice_data[3],
-                "address": invoice_data[4],
-                "invoice_value": invoice_data[5],
-                "invoiced_period": invoice_data[6],
-                "issue_date": invoice_data[7].strftime('%Y-%m-%d'),
-                "reading_date": invoice_data[8].strftime('%Y-%m-%d'),
-                "kw_cost": invoice_data[9],
-                "kw_consumed": invoice_data[10],
-                "financing": invoice_data[11],
-                "telephone": invoice_data[12],
-                "stratum": invoice_data[13],
-                "counter": invoice_data[14],
-                "user_points": invoice_data[15],
-                "cost_emergy": cost_energy,
-                "subtotal": subtotal,
-                "rounding": rounding,
-                "suspension_date": suspension_date.strftime('%Y-%m-%d'),
-                "next_reading": next_reading.strftime('%Y-%m-%d')
-            }
-            return jsonify(invoice_dict), 200
+            return jsonify(invoice_data), 200
         else:
             return jsonify({"message": "Invoice not found"}), 404
     except mysql.connector.Error as err:
         return jsonify({"error": str(err)}), 500
-    finally:
-        if 'con' in locals() and con.is_connected():
-            con.close()
+
+@app.route('/getinvoicepdf/<int:id_contract>')
+def get_invoice_pdf(id_contract):
+    try:
+        print("Generating PDF from: ", id_contract)
+        invoice_data = fetch_invoice_data(id_contract)
+        if not invoice_data:
+            return jsonify({"message": "Invoice not found"}), 404
+        print(invoice_data)
+        env = Environment(loader=FileSystemLoader('.'))
+        template = env.get_template("invoice_template.html")
+        html_content = template.render(invoice_data)
+
+        pdf = pisa.CreatePDF(html_content)
+        if not pdf.err:
+            pdf = pdf.dest.getvalue()
+            response = make_response(pdf)
+            response.headers['Content-Type'] = 'application/pdf'
+            return response
+        else:
+            return jsonify({"message": "Error generating PDF"}), 500
+    except mysql.connector.Error as err:
+        return jsonify({"error": str(err)}), 500
+
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True, port=5000)
